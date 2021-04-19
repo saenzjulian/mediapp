fb5a5c1f83b476cf6d8382fbd676656b65224766
saenzjulian <saenzjulian80@gmail.com>
2021-04-19 20:45:34 -0300

    Getter and Setter DetalleConsulta

--- mediapp-backend/src/main/java/com/mitocode/model/DetalleConsulta.java
+++ mediapp-backend/src/main/java/com/mitocode/model/DetalleConsulta.java
@@ -28,4 +28,38 @@ public class DetalleConsulta {
 	@Column(name = "tratamiento", nullable = false, length = 300)
 	private String tratamiento;
 
+	public Integer getIdDetalle() {
+		return idDetalle;
+	}
+
+	public void setIdDetalle(Integer idDetalle) {
+		this.idDetalle = idDetalle;
+	}
+
+	public Consulta getConsulta() {
+		return consulta;
+	}
+
+	public void setConsulta(Consulta consulta) {
+		this.consulta = consulta;
+	}
+
+	public String getDiagnostico() {
+		return diagnostico;
+	}
+
+	public void setDiagnostico(String diagnostico) {
+		this.diagnostico = diagnostico;
+	}
+
+	public String getTratamiento() {
+		return tratamiento;
+	}
+
+	public void setTratamiento(String tratamiento) {
+		this.tratamiento = tratamiento;
+	}
+	
+	
+
 }
