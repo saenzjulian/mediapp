4a521fd485b7dcbbddcf1fa332a3817c9c8357ba
saenzjulian <saenzjulian80@gmail.com>
2021-04-19 20:47:10 -0300

    Getter and Setter Consulta

--- mediapp-backend/src/main/java/com/mitocode/model/Consulta.java
+++ mediapp-backend/src/main/java/com/mitocode/model/Consulta.java
@@ -66,4 +66,62 @@ public class Consulta {
 	@OneToMany(mappedBy = "consulta", cascade = { CascadeType.ALL }, orphanRemoval = true)
 	private List<DetalleConsulta> detalleConsulta;
 
+	public Integer getIdConsulta() {
+		return idConsulta;
+	}
+
+	public void setIdConsulta(Integer idConsulta) {
+		this.idConsulta = idConsulta;
+	}
+
+	public Paciente getPaciente() {
+		return paciente;
+	}
+
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+
+	public Medico getMedico() {
+		return medico;
+	}
+
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+
+	public Especialidad getEspecialidad() {
+		return especialidad;
+	}
+
+	public void setEspecialidad(Especialidad especialidad) {
+		this.especialidad = especialidad;
+	}
+
+	public String getNumConsultorio() {
+		return numConsultorio;
+	}
+
+	public void setNumConsultorio(String numConsultorio) {
+		this.numConsultorio = numConsultorio;
+	}
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+
+	public List<DetalleConsulta> getDetalleConsulta() {
+		return detalleConsulta;
+	}
+
+	public void setDetalleConsulta(List<DetalleConsulta> detalleConsulta) {
+		this.detalleConsulta = detalleConsulta;
+	}
+	
+	
+
 }
