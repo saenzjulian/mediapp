40b639e2f11213c2e6aadcefc3d4471d32421a6a
saenzjulian <saenzjulian80@gmail.com>
2021-04-21 18:12:10 -0300

    error url compilation

--- mediapp-backend/src/main/java/com/mitocode/controller/PacienteController.java
+++ mediapp-backend/src/main/java/com/mitocode/controller/PacienteController.java
@@ -2,12 +2,16 @@ package com.mitocode.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,15 +42,21 @@ public class PacienteController {
 	 * @RequestBody -> para cambiar el tipo de información de JSON a Java y viceversa si es necesario
 	 */
 	@PostMapping
-	public ResponseEntity<Paciente> registrar(@RequestBody Paciente p){
+	public ResponseEntity<Paciente> registrar(@Valid @RequestBody Paciente p){
 		Paciente pac = service.registrar(p);
 		return new ResponseEntity<Paciente>(pac, HttpStatus.CREATED);
 	}
 	
 	@PutMapping
-	public ResponseEntity<Paciente> modificar(@RequestBody Paciente p) throws Exception{
-		Paciente pac = service.registrar(p);
-		return new ResponseEntity<Paciente>(pac, HttpStatus.CREATED);
+	public ResponseEntity<Paciente> modificar(@Valid @RequestBody Paciente p){
+		Paciente pac = service.modificar(p);
+		return new ResponseEntity<Paciente>(pac, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> eliminar(@PathVariable("id") Integer id) {
+		service.eliminar(id); 
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
 	}
 
 }
--- mediapp-backend/src/main/java/com/mitocode/model/Paciente.java
+++ mediapp-backend/src/main/java/com/mitocode/model/Paciente.java
@@ -5,6 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Paciente {
@@ -13,21 +17,33 @@ public class Paciente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Integer idPaciente;
 	
+	@NotNull
+	@Size(min = 3, message = "Longitud del nombre no válido. Mínimo 3")
 	@Column(name = "nombres", nullable = false, length = 70)
 	private String nombres;
-		
+	
+	@NotNull
+	@Size(min = 3, message = "Longitud del apellido no válido. Mínimo 3")
 	@Column(name = "apellidos", nullable = false, length = 70)
 	private String apellidos;
-		
+	
+	@NotNull
+	@NotEmpty
+	@Size(min = 8, max = 8, message = "DNI debe tener 8 caracteres")
 	@Column(name = "dni", nullable = false, length = 8, unique = true)
 	private String dni;
-
+	
+	@Size(min = 3, max = 150, message = "Dirección debe tener minimo 3 caracteres")
 	@Column(name = "direccion", nullable = true, length = 150)
 	private String direccion;
 	
+	@Size(min = 9, max = 9, message = "Telefono debe tener mínimo 9 caracteres")
 	@Column(name = "telefono", nullable = true, length = 9)
 	private String telefono;
 	
+	@NotNull
+	@Email(message = "Email formato incorrecto")
+	//@Pattern(regexp = "aqui_va_la_expresión_regular")
 	@Column(name = "email", nullable = true, length = 55)
 	private String email;
 
