13136d4b9aae41396f2be6bc8ae3c27576ecd07a
saenzjulian <saenzjulian80@gmail.com>
2021-04-21 15:58:11 -0300

    Verbos HTTP

--- mediapp-backend/src/main/java/com/mitocode/controller/PacienteController.java
+++ mediapp-backend/src/main/java/com/mitocode/controller/PacienteController.java
@@ -3,7 +3,12 @@ package com.mitocode.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +23,30 @@ public class PacienteController {
 	private IPacienteService service;
 	
 	@GetMapping
-	public List<Paciente> list(){
-		return service.listar();
+	public ResponseEntity<List<Paciente>> list(){
+		List<Paciente> lista = service.listar();
+		return new ResponseEntity<List<Paciente>>(lista, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Paciente> listarPorId(@PathVariable("id") Integer id) {
+		Paciente pac = service.listarPorId(id); 
+		return new ResponseEntity<Paciente>(pac, HttpStatus.OK);
+	}
+	
+	/**
+	 * @RequestBody -> para cambiar el tipo de información de JSON a Java y viceversa si es necesario
+	 */
+	@PostMapping
+	public ResponseEntity<Paciente> registrar(@RequestBody Paciente p){
+		Paciente pac = service.registrar(p);
+		return new ResponseEntity<Paciente>(pac, HttpStatus.CREATED);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Paciente> modificar(@RequestBody Paciente p) throws Exception{
+		Paciente pac = service.registrar(p);
+		return new ResponseEntity<Paciente>(pac, HttpStatus.CREATED);
 	}
 
 }
