284bef7bc378ef7bd6ab0cfcc4e91ff984047a7a
saenzjulian <saenzjulian80@gmail.com>
2021-04-25 22:16:45 -0300

    nivel 3 richardson con Hateoas

--- mediapp-backend/src/main/java/com/mitocode/controller/PacienteController.java
+++ mediapp-backend/src/main/java/com/mitocode/controller/PacienteController.java
@@ -1,10 +1,13 @@
 package com.mitocode.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,10 +18,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mitocode.exception.ModelNotFoundException;
 import com.mitocode.model.Paciente;
 import com.mitocode.service.IPacienteService;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -42,13 +49,53 @@ public class PacienteController {
 		return new ResponseEntity<Paciente>(pac, HttpStatus.OK);
 	}
 	
+	@GetMapping("/hateoas/{id}")
+	public EntityModel<Paciente> listarPorIdHateoas(@PathVariable("id") Integer id) throws Exception{
+		Paciente obj = service.listarPorId(id);
+		
+		if(obj == null) {
+			throw new ModelNotFoundException("ID no encontrado " + id);
+		}
+		
+		EntityModel<Paciente> recurso = EntityModel.of(obj);
+		
+		// localhost:8080/pacientes/4
+		/*
+		 * Voy a construir un bloque de links que es el recurso de donde saqué la información
+		 **** El localhost:8080 viene implicito 
+		 * this.getClass es el endpoint que la clase -> /pacientes
+		 * .listarPorIdHateoas -> extrae la parte dinamica de la URL, ojo NO lo estoy invocando para que se ejecute
+		 */
+		WebMvcLinkBuilder link1 = linkTo(methodOn(this.getClass()).listarPorIdHateoas(id));
+		//WebMvcLinkBuilder link2 = linkTo(methodOn(this.getClass()).listarPorIdHateoas(id));
+		
+		recurso.add(link1.withRel("paciente-recurso1"));
+		//recurso.add(link2.withRel("paciente-recurso2"));
+		
+		return recurso;
+	}
+	
 	/**
 	 * @RequestBody -> para cambiar el tipo de información de JSON a Java y viceversa si es necesario
-	 */
+	 
 	@PostMapping
 	public ResponseEntity<Paciente> registrar(@Valid @RequestBody Paciente p) throws Exception{
 		Paciente pac = service.registrar(p);
 		return new ResponseEntity<Paciente>(pac, HttpStatus.CREATED);
+	}
+	*/
+	
+	@PostMapping
+	public ResponseEntity<Paciente> registrar(@Valid @RequestBody Paciente p) throws Exception{
+		Paciente pac = service.registrar(p);
+		/**
+		 * Con el fin de obtener más información del POST REQUEST en la ruta -> localhost:8080/pacientes/id
+		 * ServletUriComponentsBuilder.fromCurrentRequest() -> 	"localhost:8080/pacientes"
+		 * .path("/{id}")	->	"/{id}" : parte dinámica
+		 * .buildAndExpand(pac.getIdPaciente()).toUri(); : Cómo poblar la parte dinámica		 * 
+		 */		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(pac.getIdPaciente()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 	
 	@PutMapping
